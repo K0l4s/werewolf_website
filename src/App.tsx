0f725d4f4f41ae301390b9a42f116e450b35585a
Huỳnh Trung Kiên <21110223@student.hcmute.edu.vn>
2025-10-23 00:16:01 +0700

    feat: ads

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import AdsContainer from "./components/ads/AdsContainer"
 import Router from "./router/Router"
-import type { RootState } from "./redux/store";
+// import type { RootState } from "./redux/store";
 
 function App() {
-  const user = useSelector((state: RootState) => state.auth.user);
-  const isPremium = user?.premium || false;
+  // const user = useSelector((state: RootState) => state.auth.user);
+  // const isPremium = user?.premium || false;
 
 
   return (
